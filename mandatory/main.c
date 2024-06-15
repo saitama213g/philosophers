f3496490fce0caeeeafe4f442f58d49e9ccbb3f4
Anouar Et-Taleb <aet-tale@c1r3p1.1337.ma>
2024-06-15 11:17:34 +0100

    ds

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: aet-tale <aet-tale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 16:04:16 by aet-tale          #+#    #+#             */
-/*   Updated: 2024/06/10 12:38:24 by aet-tale         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:43:58 by aet-tale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,18 @@
 typedef struct philo_info
 {
 	int number;
+	pthread_mutex_t *fork1;
+	pthread_mutex_t *fork2;
+	int				*locked_forks;
+	int 			time_to_die;
+	int 			time_to_sleep;
 }philo_info;
 
+
+// typedef struct t_info
+// {
+	
+// }
 
 t_data give_data(int ac, char **av)
 {
@@ -31,85 +41,84 @@ t_data give_data(int ac, char **av)
 	return data;
 }
 
-void *is_eating(void *param)
+void	*eat_sleep_think(void	*param)
 {
+	int i = 0;
 	philo_info	 *info;
-
 	info = (philo_info *)param;
-	printf("philo number %i is eating \n", info->number);
-	return (NULL);
-}
-void *is_sleeping(void *param)
-{
-	philo_info	 *info;
-
-	info = (philo_info *)param;
-	printf("philo number %i is sleeping \n", info->number);
-	return (NULL);
-}
-void *is_thinking(void *param)
-{
-	philo_info	 *info;
-
-	info = (philo_info *)param;
-	printf("philo number %i is thinking \n", info->number);
+	if (info->time_to_die == 0)
+		
+	pthread_mutex_lock(info->fork1);
+	pthread_mutex_lock(info->fork2);
+		// make time_to_die reset
+		// make_fork_locked in your own data
+		printf("philo number %i is eating \n", info->number);
+		// sleep the time of eating
+	pthread_mutex_unlock(info->fork2);
+	pthread_mutex_unlock(info->fork1);
+	// then think until forks are available to use or a philo dies
 	return (NULL);
 }
 
-philo_info give_info(int i)
+philo_info	give_info(int i, pthread_mutex_t *forks, int number_ph)
 {
 	philo_info info;
 
 	info.number = i;
+	info.fork1 = &forks[i];
+	if (i + 1 == number_ph)
+		info.fork2 = &forks[0];
 	return (info);
 }
 
-// void print_info(void *param)
-// {
-// 	printf()
-// }
 
-void make_philos(int number_ph)
+void	make_threads(pthread_t *arr_thr, pthread_mutex_t *forks, t_data *data)
 {
-	int i;
-	pthread_t *arr_thr;
-	philo_info info;
-	arr_thr = malloc(sizeof(pthread_t)*number_ph);
+	int			i;
+	philo_info	info;
 
 	i = 0;
-	while (i < number_ph)
+	while (i < data->philos)
 	{
-		info = give_info(i);
-		// print_info(&info);
-		pthread_create(&arr_thr[i], NULL, is_eating, &info);
-		pthread_create(&arr_thr[i], NULL, is_sleeping, &info);
-		pthread_create(&arr_thr[i], NULL, is_thinking, &info);
+		info = give_info(i, forks, data->philos);
+		info.time_to_die = data->time_to_die;
+		info.time_to_sleep = data->time_to_sleep;
+		pthread_create(&arr_thr[i], NULL, eat_sleep_think, &info);
 		pthread_join(arr_thr[i], NULL);
-		printf("\n");
 		i++;
 	}
 }
 
+// void make_threads(pthread_t *arr_thr, int number_ph)
+// {
+// 	int i = 0;
+// 	while (i < number_ph)
+// 	{
+		
+// 		i++;
+// 	}
+// }
+
 void philo(int ac, char **av)
 {
-	t_data	data;
+	t_data			data;
+	pthread_t		*arr_thr;
+	pthread_mutex_t	*forks;
 
 	data = give_data(ac, av);
-	make_philos(data.philos);
-	// printf("philos %i\n", data.philos);
-	// printf("time_to_eat %ld\n", data.time_to_eat);
-	// printf("time_to_die %ld\n", data.time_to_die);
-	// printf("time_to_sleep %ld\n", data.time_to_sleep);
-	// printf("eating_number %i\n", data.eating_number);
+	arr_thr = malloc(sizeof(pthread_t)*data.philos);
+	forks = malloc(sizeof(pthread_mutex_t)*data.philos);
+	make_threads(arr_thr, forks, &data);
+	// make_philos(data.philos);
 }
 
 int main(int ac, char **av)
 {
-    if (ac == 6 || ac == 5)
-    {
+	if (ac == 6 || ac == 5)
+	{
 		// check_data(ac, av);
 		philo(ac, av);
-    }else
+	}else
 		write(2, "number of params is not correct\n", 33);
 	return (0);
 }
